ef4493035fedb88481da854acbef6b3e56eb8107
Marcos Masci <marcosmasci@gmail.com>
2026-05-19 23:57:27 -0300

    chore(poker): bump fork version to 0.30.1

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ classifiers = [
 
 setup(
     name="poker",
-    version="0.30.0",
+    version="0.30.1",
     description="Poker Framework",
     long_description=Path("README.rst").read_text(),
     classifiers=classifiers,
